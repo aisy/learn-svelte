ab7666bf16792e09614fe1012c64f0ffb756b829
aisy <aisymuhammad@gmail.com>
2020-01-02 17:51:01 +0700

    create component HomeItemLists as layout in home pages

---
+++ src/components/HomeItemLists.svelte
@@ -0,0 +1,23 @@
+<script>
+  import CardItem from "./CardItem.svelte";
+</script>
+
+<style>
+  .home-list-container {
+    display: block;
+    background-color: #ff3e00;
+    padding: 40px 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+</style>
+
+<div class="home-list-container">
+  <CardItem />
+  <CardItem />
+  <CardItem />
+  <CardItem />
+  <CardItem />
+  <CardItem />
+</div>
