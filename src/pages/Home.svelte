4ae62ed0056e4912edccb5e743fb30a3de6fc153
aisy <aisymuhammad@gmail.com>
2020-01-02 08:51:54 +0700

    add pages to handle pages

---
+++ src/pages/Home.svelte
@@ -0,0 +1,52 @@
+<script>
+  let name = "Ais";
+  import NavbarHeader from "../components/NavbarHeader.svelte";
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+    margin-top: 50px;
+    background: #cb2d3e; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to right,
+      #ef473a,
+      #cb2d3e
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+      to right,
+      #ef473a,
+      #cb2d3e
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  p {
+    font-size: 20px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+
+<NavbarHeader />
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
