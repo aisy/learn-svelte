ecdf5eb13c5f4cd4b5c838db2414bfd47335c0d4
aisy <aisymuhammad@gmail.com>
2020-01-14 10:57:38 +0700

    add component introduction

---
+++ src/components/Introductions.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<style>
+  .introduction {
+    width: 100%;
+    height: 600px;
+    vertical-align: middle;
+    line-height: 200px;
+    text-align: center;
+  }
+</style>
+
+<div class="introduction">
+  <h1>Welcome</h1>
+  <p>asdasdasd</p>
+</div>
