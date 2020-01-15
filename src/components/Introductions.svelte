48fbd957d1a2c54357150694b1083742ac15089c
aisy <aisymuhammad@gmail.com>
2020-01-15 12:22:19 +0700

    add image and more style in introductions componet

--- src/components/Introductions.svelte
+++ src/components/Introductions.svelte
@@ -1,18 +1,43 @@
 <script>
-
+  import Button from "./ButtonComponent.svelte";
 </script>
 
 <style>
-  .introduction {
+  .introduction-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     width: 100%;
-    height: 600px;
-    vertical-align: middle;
-    line-height: 200px;
-    text-align: center;
+    height: 700px;
+    padding: 10px;
+  }
+  .introduction-text {
+    text-align: left;
+  }
+
+  .title {
+    font-size: 60px;
+    color: #ff3e00;
+    font-weight: 700;
+    letter-spacing: -1px;
+  }
+
+  .img-content {
+    margin-left: 100px;
   }
 </style>
 
-<div class="introduction">
-  <h1>Welcome</h1>
-  <p>asdasdasd</p>
+<div class="introduction-container">
+  <div class="introduction-text">
+    <div class="title">Form idea to Realization</div>
+    <p>
+      Visit the
+      <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+      to learn how to build Svelte apps.
+    </p>
+    <Button title="Lebih lanjut" buttonWidth="220" />
+  </div>
+  <div class="img-content">
+    <img src="img/day_off.svg" alt="img introduction" style="width:600px" />
+  </div>
 </div>
